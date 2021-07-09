c228d491a593a8b1fd05f74ecf7659f5b5e9eacc
AshishK199 <87164921+AshishK199@users.noreply.github.com>
2021-07-09 12:30:36 +0530

    Add files via upload

---
+++ Meal_Info.sql
@@ -0,0 +1,56 @@
+CREATE TABLE mytable(
+   meal_id  INTEGER  NOT NULL PRIMARY KEY 
+  ,category VARCHAR(12) NOT NULL
+  ,cuisine  VARCHAR(11) NOT NULL
+);
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1885,'Beverages','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1993,'Beverages','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2539,'Beverages','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1248,'Beverages','Indian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2631,'Beverages','Indian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1311,'Extras','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1062,'Beverages','Italian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1778,'Beverages','Italian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1803,'Extras','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1198,'Extras','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2707,'Beverages','Italian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1847,'Soup','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1438,'Soup','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2494,'Soup','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2760,'Other Snacks','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2490,'Salad','Italian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1109,'Rice Bowl','Indian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2290,'Rice Bowl','Indian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1525,'Other Snacks','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2704,'Other Snacks','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1878,'Starters','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2640,'Starters','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2577,'Starters','Thai');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1754,'Sandwich','Italian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1971,'Sandwich','Italian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2306,'Pasta','Italian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2139,'Beverages','Indian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2826,'Sandwich','Italian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2664,'Salad','Italian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2569,'Salad','Italian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1230,'Beverages','Continental');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1207,'Beverages','Continental');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2322,'Beverages','Continental');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2492,'Desert','Indian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1216,'Pasta','Italian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1727,'Rice Bowl','Indian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1902,'Biryani','Indian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1247,'Biryani','Indian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2304,'Desert','Indian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1543,'Desert','Indian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1770,'Biryani','Indian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2126,'Pasta','Italian');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1558,'Pizza','Continental');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2581,'Pizza','Continental');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1962,'Pizza','Continental');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1571,'Fish','Continental');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2956,'Fish','Continental');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2104,'Fish','Continental');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2444,'Seafood','Continental');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (2867,'Seafood','Continental');
+INSERT INTO mytable(meal_id,category,cuisine) VALUES (1445,'Seafood','Continental');
